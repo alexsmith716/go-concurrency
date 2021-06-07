446538603fda99280e921393332b96f794790b32
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2021-06-07 14:42:01 -0400

    feat: Modified example asynch and worker-pool.

--- 08-worker-pool/02-example-asynch/main.go
+++ 08-worker-pool/02-example-asynch/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 // example demonstrates asychronously processing data (no worker pool)
-// example demonstrates processing 100 simulated API calls each of which take 1/10th of a sec to complete
-// this is in contrast to '02-example-synch' and '03-example-worker-pool'
+// example demonstrates processing 1000 simulated API calls each of which take 1/10th of a sec to complete
+// this is in contrast to '01-example-synch' and '03-example-worker-pool'
 
 type apiDataType struct {
 	id int
@@ -25,33 +25,28 @@ func fetch(data apiDataType, wg *sync.WaitGroup) {
 	apiRequest(data)
 }
 
-func work(allApiCalls []apiDataType) {
+func work(allApiCalls []apiDataType, numApiCalls int) {
 	fmt.Println("start simultaneously requesting 100 APIs ------------------")
 
 	startTime := time.Now()
 
-	// object is to load (pre-load) all API calls to measure elapsed time
+	// the object here is to load (pre-load) all API calls to measure elapsed time
 	// since each API call is now its own goroutine, using 'WaitGroup' is needed
 	// 'WaitGroup' will wait for all goroutines to finish (Promise.all())
 	var wg sync.WaitGroup
 
 	// create a buffered channel with a buffer size/capacity of 100
-	// immediately write 100 integers into 'bufferedChannel' to match 'numApiCalls' (will avoid any blocking)
-	// sends to a buffered channel are blocked only when the buffer is full
-	bufferedChannel := make(chan apiDataType, 100)
+	// buffer will immediately write 100 integers into 'bufferedChannel'
+	// 'sends' to a buffered channel are blocked only when the buffer is full
+	bufferedChannel := make(chan apiDataType, numApiCalls)
 	// bufferedChannel := make(chan apiDataType)
 	// bufferedChannel := make(chan apiDataType, 3000)
-
-	// add all 'allApiCalls' to 'bufferedChannel'
-	for i := 0; i < len(allApiCalls); i++ {
-		bufferedChannel <- allApiCalls[i]
-	}
 
 	// starting 'WaitGroup' collection
 	// add 'main go func' goroutine to to 1st collection
 	wg.Add(1)
 
-	// self invoked goroutine will immediately read all data in the channel
+	// self invoked goroutine will immediately read (buffered/unbuffered) data in the channel
 	go func() {
 
 		// close WaitGroup so it can be reused by following goroutines
@@ -67,10 +62,18 @@ func work(allApiCalls []apiDataType) {
 			// increment WaitGroup by each API call
 			wg.Add(1)
 
-			// each API call add is goroutine 
+			// each API call added is a goroutine 
 			go fetch(data, &wg)
 		}
 	}()
+
+	// self invoked goroutine has now read/extracted (some) data in the channel (avoiding deadlock/panic)
+	// data has been read/extracted from the channel
+	// so now writing number of read/extracted 'allApiCalls' to 'bufferedChannel'
+	// this read/write cycle continues until the channel is closed
+	for i := 0; i < len(allApiCalls); i++ {
+		bufferedChannel <- allApiCalls[i]
+	}
 
 	close(bufferedChannel)
 
@@ -84,7 +87,7 @@ func work(allApiCalls []apiDataType) {
 func main() {
 
 	// numApiCalls := 3000
-	numApiCalls := 100
+	numApiCalls := 1000
 
 	// array of api data calls
 	var allApiCalls []apiDataType
@@ -96,9 +99,9 @@ func main() {
 	}
 
 	// call 'work' with all requests to process
-	work(allApiCalls)
+	work(allApiCalls, numApiCalls)
 }
 
-//	% go run mainB.go
+//	% go run main.go
 //	start simultaneously requesting 100 APIs ------------------
-//	total API processing time: 100.64489ms
+//	total API processing time: 103.778568ms
